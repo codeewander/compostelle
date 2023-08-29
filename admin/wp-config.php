3bc6248162628050ff1779410f51548776f58b13
Kira <changru.yang02@gmail.com>
2023-08-29 17:42:52 +0200

    feat: update environment setting

--- admin/wp-config.php
+++ admin/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'compostelle' );
 
 /** Database username */
-define( 'DB_USER', 'kira' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'kirayang' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
